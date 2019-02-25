1750698374019985180231b7ba92f735ff20b1ea
Julio Martinez <juliomb89@gmail.com>
2019-02-25 20:56:29 +0000

    Created project, added .gitignore

---
+++ Midia/Midia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Midia
+//
+//  Created by Julio Martínez Ballester on 2/25/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
