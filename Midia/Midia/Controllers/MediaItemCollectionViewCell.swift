697296aa05410728349eabca62d332d2c2bc66f9
Julio Martinez <juliomb89@gmail.com>
2019-02-26 23:03:47 +0000

    Added collection view cell, UI defined, connected outlets

---
+++ Midia/Midia/Controllers/MediaItemCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MediaItemCollectionViewCell.swift
+//  Midia
+//
+//  Created by Julio Martínez Ballester on 2/26/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import UIKit
+
+class MediaItemCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+
+}
