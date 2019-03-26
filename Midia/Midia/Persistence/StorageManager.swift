528f495d68006894b6ec3d92d8d3f892cd3aedbc
Julio Martinez <juliomb89@gmail.com>
2019-03-26 22:44:08 +0000

    persistence layer, only userdefaults

--- Midia/Midia/Persistence/StorageManager.swift
+++ Midia/Midia/Persistence/StorageManager.swift
@@ -11,6 +11,8 @@ import Foundation
 class StorageManager {
 
     //static let shared: FavoritesProvidable = UserDefaultStorageManager(withMediaItemKind: .book)
-    static let shared: FavoritesProvidable = CoreDataStorageManager(withMediaItemKind: .book)
+    //static let shared: FavoritesProvidable = CoreDataStorageManager(withMediaItemKind: .book)
+
+    static let shared = UserDefaultStorageManager(withMediaItemKind: .movie)
 
 }
